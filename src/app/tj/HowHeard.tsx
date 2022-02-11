05034f3913d45dd680ba4d172f22ee86febbc9b0
marcedevtj <87325035+marcedevtj@users.noreply.github.com>
2022-02-11 14:55:55 -0500

    updated how heard

--- src/app/tj/HowHeard.tsx
+++ src/app/tj/HowHeard.tsx
@@ -1,36 +1,28 @@
 import { Order } from '@bigcommerce/checkout-sdk';
 import React, { useEffect, useState, FunctionComponent } from 'react';
 
-const options = [
-  { name: 'Already a Customer', value: 'Already a Customer' },
-  { name: 'Ashley Tisdale', value: 'Ashley Tisdale' },
-  { name: 'Catalog \/ Mailer', value: 'Catalog \/ Mailer' },
-  { name: 'CBS The Talk', value: 'CBS The Talk' },
-  { name: 'Clay Travis & Buck Sexton', value: 'Clay Travis & Buck Sexton' },
-  { name: 'Facebook Ad', value: 'Facebook Ad' },
-  { name: 'Friends or Family Member', value: 'Friends or Family Member' },
-  { name: 'Hulu', value: 'Hulu' },
-  { name: 'Instagram Influencer', value: 'Instagram Influencer' },
-  { name: 'Jenna Dewan', value: 'Jenna Dewan' },
-  { name: 'Magazine \/ Newspaper', value: 'Magazine \/ Newspaper' },
-  { name: "Men's Wearhouse", value: "Men's Wearhouse" },
-  { name: 'Nordstrom', value: 'Nordstrom' },
-  { name: 'Online Search', value: 'Online Search' },
-  { name: 'Other', value: 'Other' },
-  { name: 'Other Department Store', value: 'Other Department Store' },
-  { name: 'Podcast', value: 'Podcast' },
-  { name: 'QVC', value: 'QVC' },
-  { name: 'Radio', value: 'Radio' },
-  { name: 'Received as Gift', value: 'Received as Gift' },
-  { name: 'Social Media', value: 'Social Media' },
-  { name: 'Specialty Store', value: 'Specialty Store' },
-  { name: 'Today Show', value: 'Today Show' },
-  { name: 'Tommy John Online', value: 'Tommy John Online' },
-  { name: 'TV Ad', value: 'TV Ad' },
-  { name: 'YouTube', value: 'YouTube' },
-  { name: 'YouTube Influencer', value: 'YouTube Influencer' },
+const optionsGroups = [
+  { name: 'Radio', options: [
+    {name: 'Mike & Mike', value: 'Mike & Mike'},
+    { name: 'Howard Stern', value: 'Howard Stern' },
+    { name: 'Colin Cowherd', value: 'Colin Cowherd' },
+    { name: 'Adam Carolla', value: 'Adam Carolla' },
+    { name: 'CNN Radio', value: 'CNN Radio' },
+    { name: 'Sirius XM', value: 'Sirius XM' },
+    { name: 'Z100', value: 'Z100' },
+  ] },
+  { name: 'Social', options: [
+    { name: 'Pinterest', value: 'Pinterest' },
+    { name: 'Facebook', value: 'Facebook' },
+    { name: 'Instagram', value: 'Instagram' },
+    { name: 'YouTube', value: 'YouTube' },
+  ] },
+  { name: 'Print', options: [
+    { name: 'Tommy John Catalog' },
+    { name: 'Delta Sky Magazine' },
+    { name: 'Golf Digest', value: 'Golf Digest' },
+  ] },
 ];
-
 interface HowHeardProps {
   order: Order;
 }
@@ -89,7 +81,13 @@ export const HowHeard: FunctionComponent<HowHeardProps> = ({ order }) => {
       <div className="field-wrapper">
         <select className="select" name="orderComment" onChange={ handleChange } value ={ selected }>
           <option disabled hidden value="">Source</option>
-          { options.map(({ name, value }) => <option key={ value } value={ value }>{ name }</option>) }
+          { optionsGroups.map(({ name: groupName, options }) => (
+            <optgroup key={ groupName } label={ groupName }>
+              { options.map(({ name, value }) => (
+                <option key={ value } value={ value }>{ name }</option>
+              )) }
+            </optgroup>
+          )) }
         </select>
 
         <button className="submit" disabled={ !selected }>Submit</button>
