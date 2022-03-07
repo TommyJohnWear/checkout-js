64cb4a3745e96b8e88fea362c4703ebb49958c54
marcedevtj <87325035+marcedevtj@users.noreply.github.com>
2022-03-07 15:02:08 -0500

    setup localStorage for text subscribe

--- src/app/shipping/TextSubscribe.tsx
+++ src/app/shipping/TextSubscribe.tsx
@@ -1,24 +1,33 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 import { CheckboxFormField } from '../ui/form';
 
-const TextSubscribe = () => (
-  <div className="form-body">
-      <div id="subscribe-to-text-wrapper">
-          <CheckboxFormField
-              id="subscribe-to-text"
-              labelContent={
-                  <>
-                      <h4 className="subscribe-heading">Sign up via text messages</h4>
-                      <p className="subscribe-subheading">Get exclusive offers and new arrivals when you sign up for texts from Tommy John.</p>
-                      <p className="subscribe-text">messages (e.g. cart reminders) from Tommy John at the cell number used when signing up. Consent is not a condition of any purchase. Reply HELP for help and STOP to cancel. Msg frequency varies. Msg & data rates may apply. View Terms & Privacy.</p>
-                  </>
-              }
-              name="subscribeToText"
-              // onChange={ onChange }
-          />
-      </div>
-  </div>
-);
+const TextSubscribe = () => {
+    const [isSubscribed, setIsSubscribed] = useState(false);
+
+    const onChange = () => {
+        localStorage.setItem('subscribeToText', (!isSubscribed).toString());
+        setIsSubscribed(prev => !prev);
+    };
+
+    return (
+    <div className="form-body">
+        <div id="subscribe-to-text-wrapper">
+            <CheckboxFormField
+                id="subscribe-to-text"
+                labelContent={
+                    <>
+                        <h4 className="subscribe-heading">Sign up via text messages</h4>
+                        <p className="subscribe-subheading">Get exclusive offers and new arrivals when you sign up for texts from Tommy John.</p>
+                        <p className="subscribe-text">messages (e.g. cart reminders) from Tommy John at the cell number used when signing up. Consent is not a condition of any purchase. Reply HELP for help and STOP to cancel. Msg frequency varies. Msg & data rates may apply. View Terms & Privacy.</p>
+                    </>
+                }
+                name="subscribeToText"
+                onChange={ onChange }
+            />
+        </div>
+    </div>
+    );
+};
 
 export default TextSubscribe;
