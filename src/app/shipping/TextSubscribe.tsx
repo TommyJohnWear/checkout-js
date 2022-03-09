79c420861afefb68b5486badbd1e5a105b3580f7
marcedevtj <87325035+marcedevtj@users.noreply.github.com>
2022-03-09 13:51:11 -0500

    updated text subscribe legal details

--- src/app/shipping/TextSubscribe.tsx
+++ src/app/shipping/TextSubscribe.tsx
@@ -3,31 +3,48 @@ import React, { useState } from 'react';
 import { CheckboxFormField } from '../ui/form';
 
 const TextSubscribe = () => {
-    const [isSubscribed, setIsSubscribed] = useState(false);
+  const [isSubscribed, setIsSubscribed] = useState(false);
 
-    const onChange = () => {
-        localStorage.setItem('subscribeToText', (!isSubscribed).toString());
-        setIsSubscribed(prev => !prev);
-    };
+  const onChange = () => {
+    localStorage.setItem('subscribeToText', (!isSubscribed).toString());
+    setIsSubscribed(prev  => !prev);
+  };
 
-    return (
+  return (
     <div className="form-body">
-        <div id="subscribe-to-text-wrapper">
-            <CheckboxFormField
-                id="subscribe-to-text"
-                labelContent={
-                    <>
-                        <h4 className="subscribe-heading">Sign up via text messages</h4>
-                        <p className="subscribe-subheading">Get exclusive offers and new arrivals when you sign up for texts from Tommy John.</p>
-                        <p className="subscribe-text">messages (e.g. cart reminders) from Tommy John at the cell number used when signing up. Consent is not a condition of any purchase. Reply HELP for help and STOP to cancel. Msg frequency varies. Msg & data rates may apply. View Terms & Privacy.</p>
-                    </>
-                }
-                name="subscribeToText"
-                onChange={ onChange }
-            />
-        </div>
+      <div id="subscribe-to-text-wrapper">
+        <CheckboxFormField
+            id="subscribe-to-text"
+            labelContent={
+            <>
+              <h4 className="subscribe-heading">Sign up for text messages</h4>
+              <p className="subscribe-subheading">
+                Get exclusive offers and new arrivals when you sign up for texts
+                from Tommy John.
+              </p>
+              <p className="subscribe-text">
+                By checking this box, you agree to receive recurring automated
+                promotional and personalized marketing text messages (e.g. cart
+                reminders) from Tommy John at the cell number used when signing
+                up. Consent is not a condition of any purchase. Reply HELP for
+                help and STOP to cancel. Msg frequency varies. Msg & data rates
+                may apply. View
+                { ' ' }
+                <a href="http://attn.tv/tommyjohn/terms.html" rel="noopener noreferrer" target="_blank">Terms</a>
+                { ' ' }
+                &
+                { ' ' }
+                <a href="https://attnl.tv/p/Z-n" rel="noopener noreferrer" target="_blank">Privacy</a>
+                .
+              </p>
+            </>
+          }
+            name="subscribeToText"
+            onChange={ onChange }
+        />
+      </div>
     </div>
-    );
+  );
 };
 
 export default TextSubscribe;
