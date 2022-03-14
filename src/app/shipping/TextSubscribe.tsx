a620403a5bb09657acef2a9ba05f4be3cc7afe17
marcedevtj <87325035+marcedevtj@users.noreply.github.com>
2022-03-14 11:18:04 -0400

    fixed error for subscribeToText

--- src/app/shipping/TextSubscribe.tsx
+++ src/app/shipping/TextSubscribe.tsx
@@ -9,7 +9,7 @@ const TextSubscribe = () => {
     isSubscribedToText.set(!isSubscribedToText.get());
   };
 
-  return localStorage.getItem('subscribeToText') === 'completed' ? <> : (
+  return localStorage.getItem('subscribeToText') === 'completed' ? <></> : (
     <div className="form-body">
       <div id="subscribe-to-text-wrapper">
         <CheckboxFormField
